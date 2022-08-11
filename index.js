585b64c292b1287421746ce39d9e390df46870a9
mdbappa0001 <abutanif366@gmail.com>
2022-08-12 03:41:51 +0600

    deleted api added

--- index.js
+++ index.js
@@ -12,31 +12,57 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
-        
+
         const toursCollection = client.db('techokids').collection('tours');
         const bookingCollection = client.db('techokids').collection('bookings');
+        const userCollection = client.db('techokids').collection('users');
 
 
-        app.get('/tour', async(req, res) => {
+        app.get('/tour', async (req, res) => {
             const query = {};
             const cursor = toursCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         });
 
-      app.post('/booking', async(req,res) =>{
-        const booking = req.body;
-        const result = await bookingCollection.insertOne(booking);
-        res.send(result);
-      })
+
+        app.get('/booking', async (req, res) => {
+            const user = req.query.user;
+            const query = { user: user };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
+        //DELETE
+        app.delete('/booking/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectID(id) };
+            const result = await bookingCollection.deleteOne(query);
+            res.send(result);
+        });
+
+
+
+        //POST
+        app.post('/booking', async (req, res) => {
+            const booking = req.body;
+            const result = await bookingCollection.insertOne(booking);
+            res.send(result);
+        })
+
+        app.get('/user', async (req, res) => {
+            const users = await userCollection.find().toArray();
+            res.send(users);
+        })
+
 
 
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -48,6 +74,6 @@ app.get('/', (req, res) => {
     res.send('Hello from travel agency')
 })
 
-app.listen(port, () =>{
+app.listen(port, () => {
     console.log(`Travel agency listening on port ${port}`)
 })
